8b4da6524edae37c57a52f948cae4888bddfbb90
Erika Yoon <erika@mayvenn.com>
2016-10-10 21:50:20 -0700

    Support channels / chatrooms.
    User can now join existing channels or chatrooms, and receive and send
    messages within this channel.
    
    TODO:
    - Allow users to create chatroom. Currently using dummy channel data so
    users can join existing ones.
    - Support message creation timestamp and sort accordingly?
    - Store username of each connection?

--- src/chat/core.clj
+++ src/chat/core.clj
@@ -5,49 +5,83 @@
             [cheshire.core :refer (generate-string parse-string)])
   (:gen-class))
 
-;; TODO
-;; use middleware for parsing and encoding json strings
+;; Current Connections
+;; {<connection1> {:connected-channel-id 1} ...}
+(def conns (atom {}))
 
-;; globals
-;; keep track of all messages
-;; and all existing channels
+;; Current Channels
 
-(def messages (atom [{:content "hi" :username "erikay"}]))
-(def channels (atom []))
+(def sample-channels {1 {:id 1
+                         :name "channel-1"
+                         :conns []
+                         :messages [{:content "Hello From 1!" :username "john1"}]}
+                      2 {:id 2
+                         :name "channel-2"
+                         :conns []
+                         :messages [{:content "Howdy from 2" :username "john2"}]}})
 
-(defn register-channel [ch]
-  (swap! channels conj ch))
+(def channels (atom sample-channels))
 
-(defn deregister-channel [ch]
-  (swap! channels (fn [channels] (remove #(= ch %) channels))))
+;; Connection Management
+(defn register-conn [conn]
+  (swap! conns assoc conn {:connected-channel-id nil}))
 
-(defn store-message [msg]
-  (swap! messages conj msg))
+(defn deregister-conn [conn]
+  (swap! conns dissoc conn))
 
-(defn send-previous-msgs [ch]
-  (async/send! ch (generate-string @messages)))
 
-(defn broadcast-message [msg]
-  (doseq [ch @channels]
-    (async/send! ch (generate-string msg))))
+;; Channel Operations
+(defn send-previous-chat-messages [conn]
+  (let [connected-channel-id (get-in @conns [conn :connected-channel-id])
+        chat-messages        (get-in @channels [connected-channel-id :messages])]
+    (async/send! conn (generate-string {:event "channel-messages"
+                                        :data  chat-messages}))))
+
+(defn publish-chat-message-to-channel [chat-message channel-id]
+  (let [channel (@channels channel-id)
+        participants (:conns channel)]
+    (doseq [conn participants]
+      (async/send! conn (generate-string {:event "new-message"
+                                          :data chat-message})))))
+
+(defn handle-join-channel [conn channel-id]
+  (let [prev-channel-id (get-in @conns [conn :connected-channel-id])]
+    (swap! channels update-in [prev-channel-id :conns]
+           (fn [conns] (remove #(= % conn) conns)))
+    (swap! channels update-in [channel-id :conns] #(conj % conn))
+    (swap! conns assoc-in [conn :connected-channel-id] channel-id)
+    (send-previous-chat-messages conn)))
+
+(defn handle-add-message [conn chat-message]
+  (let [channel-id (get-in @conns [conn :connected-channel-id])]
+    (swap! channels update-in [channel-id :messages] #(conj % chat-message))
+    (publish-chat-message-to-channel chat-message channel-id)))
 
 (def websocket-callbacks
-  {:on-open (fn [ch]
-              (register-channel ch)
-              (send-previous-msgs ch))
-   :on-message (fn [ch message]
-                 (println "received " message)
-                 (let [msg (parse-string message)]
-                   (store-message msg)
-                   (broadcast-message msg)))
-   :on-error (fn [ch err]
-               (println "error!"))
-   :on-close (fn [ch {:keys [code reason]}]
-               (deregister-channel ch)
-               (println "closing connection!"))})
+  (letfn [(handle-connection [conn]
+            (let [data {:event "connected"
+                        :data  {}}]
+              (async/send! conn (generate-string data))))
+
+          (handle-message [conn message]
+            (let [message'             (parse-string message true)
+                  {:keys [event data]} message']
+              (condp = event
+                "join-channel" (handle-join-channel conn (:id data))
+                "add-message"  (handle-add-message conn data))))
+
+          (handle-error [conn err]
+            (println "error!"))
+
+          (handle-on-close [conn {:keys [code reason]}]
+            (deregister-conn conn)
+            (println "closing connection due to code " code ", reason: "  reason))]
+    {:on-open    handle-connection
+     :on-message handle-message
+     :on-error   handle-error
+     :on-close   handle-on-close}))
 
 (defn app [request]
-  (println request)
   (async/as-channel request websocket-callbacks))
 
 (defn -main [& {:as args}]
